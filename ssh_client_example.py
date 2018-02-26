71f73685844e9613734b3cc2195b9bf0720cfd2a
mrg5103 <35038722+mrg5103@users.noreply.github.com>
2018-02-26 12:55:23 -0500

    Update ssh_client_example.py

--- ssh_client_example.py
+++ ssh_client_example.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
+
 
 import paramiko
 
 hostname = 'localhost'
 port = 22
-username = 'foo'
-password = 'xxxYYYxxx'
+username = 'example'
+password = 'password'
 
 if __name__ == "__main__":
     paramiko.util.log_to_file('paramiko.log')
     s = paramiko.SSHClient()
     s.load_system_host_keys()
     s.connect(hostname, port, username, password)
-    stdin, stdout, stderr = s.exec_command('ifconfig')
+    stdin, stdout, stderr = s.exec_command('ifconfig')  #prints ifconfig and exits, can be cusotmised
     print stdout.read()
     s.close()
